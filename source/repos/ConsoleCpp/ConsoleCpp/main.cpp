c771c7cd10fb7097cd5f6b7ebb634de0a0fe76c0
doctop <doctop@mail.ru>
2025-09-25 23:07:33 +0300

    Первый коммит: добавлен проект ConsoleCpp

---
+++ source/repos/ConsoleCpp/ConsoleCpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	
+
+	int i;
+
+	cout << "введи число: " << endl;
+	cin >> i;
+	cout << "твое число -> " << i << endl << endl;
+	//000dsf66757=
+
+
+
+
+	return 0;
+}
